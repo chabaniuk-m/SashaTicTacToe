cd56eba5fe7a659a028ec42b82d5d157cc25c446
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-02-28 19:50:44 +0200

    Changes by Asus

--- src/org/game/player/Player.java
+++ src/org/game/player/Player.java
@@ -1,4 +1,23 @@
 package org.game.player;
 
+import org.game.Board;
+
 public abstract class Player {
+    private final char mark;
+    /*
+    comment
+     */
+
+    public Player(char mark) {
+        this.mark = mark;
+    }
+
+    abstract public void choosePosition(Board board);
+
+    public char getMark() {
+        return mark;
+    }
+
+
+    abstract public String  winMessage();
 }
--- src/org/game/player/UserPlayer.java
+++ src/org/game/player/UserPlayer.java
@@ -1,4 +1,21 @@
 package org.game.player;
 
+import org.game.Board;
+
 public class UserPlayer extends Player {
+    String name;
+
+    public UserPlayer() {
+        super('O');
+    }
+
+    @Override
+    public void choosePosition(Board board) {
+        // TODO: (user) choosePosition(board)
+    }
+
+    @Override
+    public String winMessage() {
+        return "User wins!";
+    }
 }
